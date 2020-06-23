461557140dfd953bc9e5287ad4186df0a0596283
ACselvan <66485390+ACselvan@users.noreply.github.com>
2020-06-23 22:11:35 +0530

    23/06/2020

---
+++ app/src/main/java/com/e/myapplication/Matrimony_info_demo.java
@@ -0,0 +1,14 @@
+package com.e.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Matrimony_info_demo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrimony_info_demo);
+    }
+}
